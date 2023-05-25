f7f5e01e5594b662ec4620a658022360544bfc49
SANDRA DIAMANTINA CAMPOS VALDES <you@example.com>
2023-05-25 09:55:15 -0600

    first commit

--- app.config.js
+++ app.config.js
@@ -1,4 +1,6 @@
-{
+import 'dotenv/config';
+
+export default{
   "expo": {
     "name": "Chat",
     "slug": "Chat",
@@ -25,6 +27,17 @@
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    extra:{
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appID: process.env.APP_ID
+
+
+    }
     }
   }
-}
+
